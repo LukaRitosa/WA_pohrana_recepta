94f52ef01ff358d0eabb42e2d9fc7a7e4f2cd31a
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-11-07 12:04:26 +0100

    commit

--- app/routes/korisnici.js
+++ app/routes/korisnici.js
@@ -9,4 +9,54 @@ router.get('/', (req, res)=>{
 })
 
 
+router.get('/:id', (req, res)=>{
+    const id_korisnik=req.params.id
+
+    const korisnik=korisnici.find(k => k.id == id_korisnik)
+
+    if(!korisnik){
+        return res.status(401).json({greska: 'Ne postoji korisnik kojeg pokušavate pronaći'})
+    }
+
+    return res.status(200).json(korisnik)
+})
+
+router.post('/', (req, res)=>{
+    const novi_korisnik= req.body
+
+    const dozvoljeni_kljucevi= ['ime', 'prezime', 'email']
+
+    const kljucevi_korisnika=Object.keys(novi_korisnik)
+
+    const krivi_kljucevi= kljucevi_korisnika.some(k => !dozvoljeni_kljucevi.includes(k))
+
+    if(krivi_kljucevi){
+        return res.status(400).json({greska: 'Nepravilan oblik korisnika'})
+    }
+
+    const novi_id= korisnici.at(-1)['id'] + 1 
+
+    korisnici.push({id: novi_id, ... novi_korisnik})
+
+    return res.status(201).json(korisnici)
+})
+
+router.delete('/:id', (req, res)=>{
+    const id_korisnik= req.params.id
+
+    const index= korisnici.findIndex(k => k.id == id_korisnik)
+
+    if(index == -1){
+        return res.status(404).json({greska: 'Ne postoji korisnik kojeg pokušavate obrisati'})
+    }
+
+    console.log(korisnici)
+
+    korisnici.splice(index, 1)
+
+    console.log(korisnici)
+
+    return res.status(204).json({odgovor: 'Korisnik obrisan'})
+})
+
 export default router
